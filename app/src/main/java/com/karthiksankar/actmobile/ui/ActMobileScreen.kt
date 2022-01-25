badb517cbe1b179178509f8e4ddb2074ae8f425c
Karthik Sankar <thedukerchip@gmail.com>
2022-01-26 01:07:14 +0530

    feat: add screen composable with Theme + Surface

---
+++ app/src/main/java/com/karthiksankar/actmobile/ui/ActMobileScreen.kt
@@ -0,0 +1,22 @@
+package com.karthiksankar.actmobile.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.karthiksankar.actmobile.ui.theme.ActMobileTheme
+
+@Composable
+fun ActMobileScreen(
+    content: @Composable () -> Unit
+) {
+    ActMobileTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
+    }
+}
